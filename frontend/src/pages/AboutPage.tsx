9ffc483e2e1c68c6d5001c59c8720d5979d95eed
Vladimir Rodionov <varodionov@phoenixit.ru>
2025-03-22 10:39:44 +0300

    fix missing translations, simplify about page

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -21,48 +21,6 @@ export const AboutPage: React.FC = () => {
             <Typography variant="body1" paragraph>
               {t('about.description')}
             </Typography>
-            <Divider sx={{ my: 2 }} />
-            <Typography variant="h6" gutterBottom>
-              {t('about.features')}
-            </Typography>
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <Assignment />
-                </ListItemIcon>
-                <ListItemText 
-                  primary={t('about.task_management')} 
-                  secondary={t('about.task_management_desc')}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <Category />
-                </ListItemIcon>
-                <ListItemText 
-                  primary={t('about.categorization')} 
-                  secondary={t('about.categorization_desc')}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <PriorityHigh />
-                </ListItemIcon>
-                <ListItemText 
-                  primary={t('about.priorities')} 
-                  secondary={t('about.priorities_desc')}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <Schedule />
-                </ListItemIcon>
-                <ListItemText 
-                  primary={t('about.deadlines')} 
-                  secondary={t('about.deadlines_desc')}
-                />
-              </ListItem>
-            </List>
           </CardContent>
         </Card>
 
