611e13a0aea4d1c7d56fd57f3b6e8b2ac7199dd7
Vladimir Rodionov <varodionov@phoenixit.ru>
2025-04-05 19:52:20 +0300

    UpdateTaskDto date

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -87,7 +87,7 @@ export interface UpdateTaskDto {
     status_id?: number;
     priority_id?: number;
     duration_id?: number;
-    deadline?: string;
+    deadline?: Date;
     completed?: boolean;
 }
 
