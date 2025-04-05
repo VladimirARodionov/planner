7c97962163a5a2da7db362902960b94fff6a572b
Vladimir Rodionov <varodionov@phoenixit.ru>
2025-04-05 19:56:35 +0300

    handleSubmit date

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -76,7 +76,7 @@ export interface CreateTaskDto {
     status_id?: number;
     priority_id?: number;
     duration_id?: number;
-    deadline?: string;
+    deadline?: Date;
     completed?: boolean;
 }
 
